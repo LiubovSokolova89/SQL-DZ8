88423e2f05c2ec94d5f22b66f97d0551ac218139
LiubovSokolova89 <liubow.sokolova@gmail.com>
2022-12-06 13:40:37 +0100

    Initial commit

---
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -0,0 +1,29 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("Should successfully login")
+    void shouldSuccessfullyLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+}
